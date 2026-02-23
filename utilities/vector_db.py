fdeeecce6f6cf1756305e5ef2599dea826b5bde8
jonah lewis <jonahnlewis4@gmail.com>
2026-02-23 03:50:33 +0000

    remove docker compose setting of certain api keys and defer that to the .env

--- utilities/vector_db.py
+++ utilities/vector_db.py
@@ -4,9 +4,10 @@ import os
 
 class VectorDb():
     def __init__(self, index_name: str = "adbm"):
-        load_dotenv(find_dotenv(), override=True)
-        # self.pc = Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
-        self.pc = Pinecone(api_key="pcsk_udGQD_Juk7zx5VqT6oyJpFsJc9WQbdwFjf8Yj2v9VSZEjyTP9aYsA6Dtt8xwCa7gC9VBi")
+        #load_dotenv(find_dotenv(), override=True)
+        key = os.getenv('PINECONE_API_KEY')
+        print('PINECONE_API_KEY', key)
+        self.pc = Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
 
         try:
             self.pc.create_index(
